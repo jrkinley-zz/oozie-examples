069dccef3a9ac48a098c2ee334b0d6e1435c69c6
James Kinley <jamesrobertkinley@gmail.com>
2012-08-13 09:34:23 +0100

    Updated comments and javadoc

--- src/main/java/com/cloudera/examples/oozie/action/MapReduceCustomAction.java
+++ src/main/java/com/cloudera/examples/oozie/action/MapReduceCustomAction.java
@@ -1,5 +1,23 @@
 package com.cloudera.examples.oozie.action;
 
-public class MapReduceCustomAction {
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.oozie.action.ActionExecutorException;
+import org.apache.oozie.action.hadoop.MapReduceActionExecutor;
+import org.apache.oozie.client.WorkflowAction;
+
+/**
+ * Creates a custom MapReduce action by extending MapReduceActionExecutor
+ * http://incubator.apache.org/oozie/docs/3.1.3/docs/DG_CustomActionExecutor.html
+ */
+public class MapReduceCustomAction extends MapReduceActionExecutor {
+
+  /**
+   * You can set "mapred.job.name" by overriding submitLauncher
+   */
+  @Override
+  public void submitLauncher(FileSystem actionFs, Context context,
+      WorkflowAction action) throws ActionExecutorException {
+    super.submitLauncher(actionFs, context, action);
+  }
 
 }
